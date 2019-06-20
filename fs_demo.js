363a89eb7b85bf455bd21b48d248d176e152dcc5
Vectormike40 <victor.junior40@yahoo.com>
2019-06-20 17:01:33 +0100

    File System Module

---
+++ fs_demo.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+
+// Create a Folder 
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created...');
+// });
+
+// // Create and write to a file
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 
+//     'Hello World', 
+//         err => {
+//         if (err) throw err;
+//             console.log('File written to...')
+
+//     // File append
+//     fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),
+//     'Hi there',
+//         err => {
+//             if (err) throw err;
+//                 console.log('File appended');
+//         }
+//     )
+// })
+
+// Rename file
+// fs.rename(
+//     path.join(__dirname, '/test', 'hello.txt'),
+//     path.join(__dirname, '/test', 'helloworld.txt'),
+//     err => {
+//         if (err) throw err;
+//         console.log('File renamed')
+//     }
+// )
