780baf406b624626d38ef16010e2a737aa45ba73
Vectormike40 <victor.junior40@yahoo.com>
2019-06-24 12:15:30 +0100

    Done

--- index.js
+++ index.js
@@ -11,14 +11,25 @@ const server = http.createServer((req, res) => {
     if(req.url === '/') {
         fs.readFile(path.join(__dirname, '/public', 'index.html'), (err, data) => {
             if (err) throw err;
+            res.writeContinue(200, {'Content-Type': 'text/html'})
             res.end(data);
         });
     }
+    // if(req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, '/public', 'about.html'), (err, data) => {
+    //         if (err) throw err;
+    //         res.end(data);
+    //     });
+    // }
+    
+    // Sending api 
     if(req.url === '/about') {
-        fs.readFile(path.join(__dirname, '/public', 'about.html'), (err, data) => {
-            if (err) throw err;
-            res.end(data);
-        });
+        const users = [
+            {name: 'Mike Jonah', age: 20},
+            {name: 'John Doe', age: 40}
+        ]
+        res.writeContinue(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(users));
     } 
 });
 
