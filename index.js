10e947c8120377e91313ce06ad6a7c97df67f285
Vectormike40 <victor.junior40@yahoo.com>
2019-06-24 11:50:05 +0100

    Creating a server

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const Logger = require('./logger');
+// const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+// const logger = new Logger();
+// logger.on('message', data => console.log('Called Listener', data))
+// logger.log('Hello World!')
 
-logger.on('message', data => console.log('Called Listener', data))
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        fs.readFile(path.join(__dirname, '/public', 'index.html'), (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    }
+    if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, '/public', 'about.html'), (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    } 
+});
 
-logger.log('Hello World!')
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- logger.js
+++ logger.js
@@ -1,4 +1,4 @@
-const EventEmitter = require('events');
+const EventEmitter from require('events');
 const uuid = require('uuid')
 
 class Logger extends EventEmitter {
